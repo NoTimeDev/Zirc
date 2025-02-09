6344e2c9ae58b823f95566f80e39fb167bf87704
Devvy <devaughntimol@gmail.com>
2025-02-08 22:02:44 -0500

    Final Push HopeFully

--- Buildfn.py
+++ Buildfn.py
@@ -9,7 +9,7 @@ import psutil
 print("l")
 for proc in psutil.process_iter(['pid', 'name', 'cmdline']):
     if proc.info['name'] == "zirc.exe":
-        proc.termiate()
+        proc.terminate()
 
 for proc in psutil.process_iter(['pid', 'name', 'cmdline']):
     if proc.info['name'] == 'python.exe' and proc.info['cmdline'] and proc.info['cmdline'][1] == 'Build.py':
